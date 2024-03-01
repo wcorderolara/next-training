6ad162fc9a283fe9f4319b799d9b7d65b5b8a006
Walter Cordero <wcordero.lara@gmail.com>
2024-02-29 21:48:48 -0600

    add auth with clerk and routing with app routing

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AskQuetion = () => {
+  return <div>AskQuetion</div>;
+};
+
+export default AskQuetion;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Next.js 14 we are comming!</h1>
-      <h2 className="h2-bold">Next.js 14 we are comming!</h2>
-      <h3 className="h3-bold">Next.js 14 we are comming!</h3>
-    </div>
-  );
-}
